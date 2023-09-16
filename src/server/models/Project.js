c51583c0a97b934258eeb62b798ab09b33b54849
kchilds1 <kenneth.childs20@gmail.com>
2023-09-16 12:47:04 -0500

    Created Projects model

---
+++ src/server/models/Project.js
@@ -0,0 +1,9 @@
+import mongoose, { model } from "mongoose"
+const ProjectSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  starts: { type: Date, required: true },
+  ends: { type: Date, required: true },
+  statuses: [String],
+});
+export default mongoose.models.Project ||
+  mongoose.model("Project", ProjectSchema, "projects");
