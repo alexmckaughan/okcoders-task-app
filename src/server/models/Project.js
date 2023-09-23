9ba8965fe63ea8edbba65182ea3755ff8b332134
alexmckaughan <alexmckaughan@yahoo.com>
2023-09-23 10:26:44 -0500

    Correcting typo/structure

--- src/server/models/Project.js
+++ src/server/models/Project.js
@@ -1,12 +1,9 @@
-import mongoose, { model } from "mongoose"
+import mongoose, { model } from "mongoose";
 const ProjectSchema = new mongoose.Schema({
   title: { type: String, required: true },
   starts: { type: Date, required: true },
   ends: { type: Date, required: true },
-  statuses:  {
-    type: [String],
-    required: true,
-  },
+  statuses: { type: String, required: true },
 });
 export default mongoose.models.Project ||
   mongoose.model("Project", ProjectSchema, "projects");
