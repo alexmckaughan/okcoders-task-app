85104c9473b62639dd9299e07f685a3f06902535
kchilds1 <kenneth.childs20@gmail.com>
2023-09-25 15:12:06 -0500

    Removed Login import from pages.index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import Tasks from "./projects";
-import Login from "./Login";
+
 export default function Home() {
   return (
     <>
-      <Login />
-      {/* <Tasks /> */}
+      
+       {/* <Tasks />  */}
     </>
   );
 }
