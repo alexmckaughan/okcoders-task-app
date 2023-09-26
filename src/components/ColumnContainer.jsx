35aa51114b17c4cc00d8f52b3fe93ae3c9051e06
alexmckaughan <alexmckaughan@yahoo.com>
2023-09-25 23:31:54 -0500

    added stack and spacing

--- src/components/ColumnContainer.jsx
+++ src/components/ColumnContainer.jsx
@@ -5,6 +5,7 @@ import {
   Grid,
   Paper,
   Button,
+  Stack,
 } from "@mui/material";
 import { TaskCard } from "./TaskCard";
 import { useRouter } from "next/router";
@@ -36,27 +37,29 @@ export function ColumnContainer(props) {
           {props.column.label}
           <hr />
         </Typography>
-        {props.tasks.map((task) => (
-          <Box key={task._id}>
+        <Stack spacing={1} >
+          {props.tasks.map((task) => (
+            <Box key={task._id}>
+              <TaskCard
+                project={props.project}
+                status={props.status}
+                task={task}
+                fetchTasks={props.fetchTasks}
+                onDelete={handleDeleteTask}
+              />
+            </Box>
+          ))}
+          {showNewTask && (
             <TaskCard
+              isNewTask
               project={props.project}
               status={props.status}
-              task={task}
-              fetchTasks={props.fetchTasks}
-              onDelete={handleDeleteTask}
+              onCreate={handleCreateNewTask}
+              onCancel={handleCancelNewTask}
             />
-          </Box>
-        ))}
-        {showNewTask && (
-          <TaskCard
-            isNewTask
-            project={props.project}
-            status={props.status}
-            onCreate={handleCreateNewTask}
-            onCancel={handleCancelNewTask}
-          />
-        )}
-        <Button onClick={() => setShowNewTask(true)}>ADD</Button>
+          )}
+          <Button onClick={() => setShowNewTask(true)}>ADD</Button>
+        </Stack>
       </Paper>
     </Grid>
   );
