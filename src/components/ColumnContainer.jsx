8af0720d8b2b813e2e42f63bb4e385095d159024
alexmckaughan <alexmckaughan@yahoo.com>
2023-09-28 20:47:36 -0500

    wrapping columncontainer in sortablecontext

--- src/components/ColumnContainer.jsx
+++ src/components/ColumnContainer.jsx
@@ -9,6 +9,7 @@ import {
 } from "@mui/material";
 import { TaskCard } from "./TaskCard";
 import { useRouter } from "next/router";
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 
 export function ColumnContainer(props) {
   const router = useRouter();
@@ -31,37 +32,39 @@ export function ColumnContainer(props) {
   };
 
   return (
-    <Grid key={props.column._id} item xs={12} sm={4}>
-      <Paper elevation={3} sx={props.columnStyles.column}>
-        <Typography variant="h4">
-          {props.column.label}
-          <hr />
-        </Typography>
-        <Stack spacing={1} >
-          {props.tasks.map((task) => (
-            <Box key={task._id}>
+    <SortableContext strategy={verticalListSortingStrategy}>
+      <Grid key={props.column._id} item xs={12} sm={4}>
+        <Paper elevation={3} sx={props.columnStyles.column}>
+          <Typography variant="h4">
+            {props.column.label}
+            <hr />
+          </Typography>
+          <Stack spacing={1} >
+            {props.tasks.map((task) => (
+              <Box key={task._id}>
+                <TaskCard
+                  project={props.project}
+                  status={props.status}
+                  task={task}
+                  fetchTasks={props.fetchTasks}
+                  onDelete={handleDeleteTask}
+                />
+              </Box>
+            ))}
+            {showNewTask && (
               <TaskCard
+                isNewTask
                 project={props.project}
                 status={props.status}
-                task={task}
-                fetchTasks={props.fetchTasks}
-                onDelete={handleDeleteTask}
+                onCreate={handleCreateNewTask}
+                onCancel={handleCancelNewTask}
               />
-            </Box>
-          ))}
-          {showNewTask && (
-            <TaskCard
-              isNewTask
-              project={props.project}
-              status={props.status}
-              onCreate={handleCreateNewTask}
-              onCancel={handleCancelNewTask}
-            />
-          )}
-          <Button onClick={() => setShowNewTask(true)}>ADD</Button>
-        </Stack>
-      </Paper>
-    </Grid>
+            )}
+            <Button onClick={() => setShowNewTask(true)}>ADD</Button>
+          </Stack>
+        </Paper>
+      </Grid>
+    </SortableContext>
   );
 }
 
